43057efc8ec6732bfa21b6d1f85bb7b47cb0cd4c
elliot-garnero <elliot84@hotmail.fr>
2020-05-27 18:41:49 +0200

    add photo and mail list

--- components/UserList.js
+++ components/UserList.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import axios from 'axios';
+export default class UserList extends React.Component {
 
-export default function UserList() {
-  return (
-    <View style={styles.container}>
-      <p>member list</p>
-    </View>
-  );
+    state = {
+    nameList: []
+    }
+
+    componentDidMount() {
+        axios.get('http://snapi.epitech.eu/all', {
+            headers: {
+                "token": "BRHtEJPd3N3Za3VMDmCDEPST"
+            }
+        })
+        .then(res => {
+            const nameList = res.data;
+            this.setState({ nameList: nameList.data });
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    render() {
+        const {nameList} = this.state;
+
+        return (
+            <View>
+                { 
+                nameList.map(user => <Text style={styles.user}>{user.email}</Text>)
+                }
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F8F8F8',
-  }
-});
+    user: {
+        marginTop: 16,
+        paddingVertical: 8,
+        borderWidth: 4,
+        borderColor: "#20232a",
+        borderRadius: 6,
+        backgroundColor: "#FFFFFF",
+        color: "#20232a",
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+  });
+  
